66aa90b8ab8fa7ea9bb5cf27e57c726e0a51e851
Edsson <akairu@hotmail.com>
2022-08-07 17:29:39 -0500

    global state - lifting state up

--- lib/widgets/tasks_tiles.dart
+++ lib/widgets/tasks_tiles.dart
@@ -1,44 +1,61 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, prefer_const_constructors_in_immutables
 
 import 'package:flutter/material.dart';
 
-class TasksTile extends StatelessWidget {
+class TasksTile extends StatefulWidget {
   const TasksTile({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<TasksTile> createState() => _TasksTileState();
+}
+
+class _TasksTileState extends State<TasksTile> {
+  bool isChecked = false;
+
+  void checkboxCallback(bool? checkboxState) {
+    setState(() {
+      isChecked = checkboxState ?? true;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('This is a task'),
-      trailing: TaskCheckbox(),
+      title: Text(
+        'This is a task',
+        style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough : null,
+        ),
+      ),
+      trailing: TaskCheckbox(
+        checkBoxState: isChecked,
+        toggleCheckboxState: checkboxCallback,
+      ),
     );
   }
 }
 
-class TaskCheckbox extends StatefulWidget {
-  const TaskCheckbox({
-    Key? key,
-  }) : super(key: key);
+class TaskCheckbox extends StatelessWidget {
+  final bool checkBoxState;
+  final Function(bool?) toggleCheckboxState;
+  TaskCheckbox(
+      {required this.checkBoxState, required this.toggleCheckboxState});
 
-  @override
-  State<TaskCheckbox> createState() => _TaskCheckboxState();
-}
-
-class _TaskCheckboxState extends State<TaskCheckbox> {
-  bool isChecked = false;
   @override
   Widget build(BuildContext context) {
     return Checkbox(
       activeColor: Colors.lightBlueAccent,
-      value: isChecked,
-      onChanged: (newValue) {
-        setState(
-          () {
-            isChecked = newValue!;
-          },
-        );
-      },
+      value: checkBoxState,
+      onChanged: toggleCheckboxState,
+      //(newValue) {
+      // setState(
+      //   () {
+      //     isChecked = newValue!;
+      //   },
+      // );
+      //},
     );
   }
 }
