9124cab424f5a768d8c958609ceefd438e1bf16d
Edsson <akairu@hotmail.com>
2022-08-07 16:57:37 -0500

    state management local state

--- lib/widgets/tasks_tiles.dart
+++ lib/widgets/tasks_tiles.dart
@@ -11,10 +11,34 @@ class TasksTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListTile(
       title: Text('This is a task'),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (bool? value) {},
-      ),
+      trailing: TaskCheckbox(),
+    );
+  }
+}
+
+class TaskCheckbox extends StatefulWidget {
+  const TaskCheckbox({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<TaskCheckbox> createState() => _TaskCheckboxState();
+}
+
+class _TaskCheckboxState extends State<TaskCheckbox> {
+  bool isChecked = false;
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: isChecked,
+      onChanged: (newValue) {
+        setState(
+          () {
+            isChecked = newValue!;
+          },
+        );
+      },
     );
   }
 }
