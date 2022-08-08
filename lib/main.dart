5b2d4080add1e9c2e2b772e1b74e978e2b1ca146
Edsson <akairu@hotmail.com>
2022-08-07 20:14:56 -0500

    provider basic usage

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,56 @@
 // ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final String data = 'Data from Parent';
+  final String data = 'Data from Parent???';
   const MyApp();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Container(child: Text(data),),
+    return Provider<String>(
+      create: ((context) => data),
+      child: MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Container(
+              child: Text(data),
+            ),
+          ),
+          body: Level1(),
         ),
-        body: Level1(data),
       ),
     );
   }
 }
 
 class Level1 extends StatelessWidget {
-  final String data;
-  const Level1(this.data);
-
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Level2(data),
+      child: Level2(),
     );
   }
 }
 
 class Level2 extends StatelessWidget {
-  final String data;
-  const Level2(this.data);
-
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Level3(data),
+      child: Level3(),
     );
   }
 }
 
 class Level3 extends StatelessWidget {
-  final String data;
-
-  const Level3(this.data);
-
   @override
   Widget build(BuildContext context) {
-    return Text(data);
+    return Text(Provider.of<String>(context));
   }
 }
 // // ignore_for_file: prefer_const_constructors
