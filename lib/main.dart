0b904ec5e82f993bbe9dfadc369c94308f73f975
Edsson <akairu@hotmail.com>
2022-08-07 20:06:08 -0500

    state management - nightmare

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers
 
 import 'package:flutter/material.dart';
-import 'package:flutter_project10_state_management/screens/tasks_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,11 +9,66 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: TasksScreen(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Container(),
+        ),
+        body: Level1(),
+      ),
     );
   }
 }
+
+class Level1 extends StatelessWidget {
+  const Level1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Level2(),
+    );
+  }
+}
+
+class Level2 extends StatelessWidget {
+  const Level2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Level3(),
+    );
+  }
+}
+
+class Level3 extends StatelessWidget {
+  const Level3({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Final Data');
+  }
+}
+// // ignore_for_file: prefer_const_constructors
+
+// import 'package:flutter/material.dart';
+// import 'package:flutter_project10_state_management/screens/tasks_screen.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: TasksScreen(),
+//     );
+//   }
+// }
