a707e6e25f208f7a0ceae0868669bc282c468edc
Edsson <akairu@hotmail.com>
2022-08-14 19:54:59 -0500

    refactor - make it private - advanced oop

--- lib/modules/task_data.dart
+++ lib/modules/task_data.dart
@@ -1,20 +1,27 @@
+import 'dart:collection';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter_project10_state_management/modules/task.dart';
 
 class TaskData extends ChangeNotifier {
-  List<Task> tasks = [
+  final List<Task> _tasks = [
     Task(name: 'Buy milk'),
     Task(name: 'Buy eggs'),
     Task(name: 'Buy bread'),
   ];
 
+// this will force you use the correct methods to add tasks
+  UnmodifiableListView<Task> get tasks {
+    return UnmodifiableListView(_tasks);
+  }
+
   int get taskCout {
-    return tasks.length;
+    return _tasks.length;
   }
 
   void addTask(String newTaskTitle) {
     final task = Task(name: newTaskTitle);
-    tasks.add(task);
+    _tasks.add(task);
     notifyListeners();
   }
 }
